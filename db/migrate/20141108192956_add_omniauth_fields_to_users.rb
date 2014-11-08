f49fe25fb97809ec696041cd1ed5bed7b598ee32
andresbravog <andresbravog@gmail.com>
2014-11-08 21:24:19 +0100

    Starting sing in with linkedin

---
+++ db/migrate/20141108192956_add_omniauth_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddOmniauthFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+
+    add_index :users, [:provider, :uid]
+  end
+end
